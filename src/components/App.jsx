10acb4f862d04cc03303bbbc153aef486df381a0
Roxie Charchian <rmcharchian@gmail.com>
2018-01-04 10:39:03 -0600

    added App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="title">
+                Reminder Pro
+                </div>
+                <div className="form-inline">
+                    <div className="form-group">
+                    <input
+                        className="form-control"
+                        placeholder="I have to..."
+                        />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
