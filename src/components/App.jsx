4f75aaaacf96334712da52df6c2427b64bc518ff
Roxie Charchian <rmcharchian@gmail.com>
2018-01-10 06:41:55 -0600

    trying to add reminder object - broken

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
+
 
 
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder(){
+        console.log('this.state', this.state);
+    }
+
     render() {
         return (
             <div className="App">
@@ -15,11 +30,13 @@ class App extends Component {
                     <input
                         className="form-control"
                         placeholder="I have to..."
+                        onChange={event => this.setState({text: event.target.value})}
                         />
                     </div>
                     <button
                         type="button"
                         className="btn btn-success"
+                        onClick={() => this.addReminder()}
                         >
                         Add Reminder
                         </button>
@@ -29,4 +46,8 @@ class App extends Component {
     }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators ({addReminder}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps) (App);
